8efe56bd6001137d111af6cccc1bf52f7d848905
Abhishek Kumar <119596164+Abhishek-Kumar-FunctionUp@users.noreply.github.com>
2023-03-02 09:39:49 +0530

    Weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./index.css";
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const apiKey = "f56f24967aaf51182d1d4df628297c6d"
+
+  async function searchLocation() {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location} &appid=${apiKey}`);
+    const city = await res.json();
+    setData(city);
+    setLocation("")
+  }
+
+  function handleCapture(e) {
+    setLocation(e.target.value);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className="navbar">
+    ⛅Weather
+    </nav>
+      <input
+        className="search"
+        type="text"
+        placeholder="Enter Location"
+        onChange={handleCapture}
+        onClick={searchLocation}
+        value={location}
+      />
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            <h1>{data.main?.temp} °F</h1>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="max">
+            <p>{data.main?.temp_max} °F</p>
+          </div>
+          <div className="min">
+            <p>{data.main?.temp_min} °F</p>
+          </div>
+          <div className="humidity">{data.main?.humidity} %</div>
+        </div>
+      </div>
     </div>
   );
 }
